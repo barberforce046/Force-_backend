01a47f4c9ea43a15f28839ce3756ee3181d75a05
JD60721 <ps3juandiego@gmail.com>
2025-11-26 23:02:46 -0500

    ajuste verce}

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ dotenv.config();
 export const port = process.env.PORT || 5000;
 export const jwtSecret = process.env.JWT_SECRET || "dev_secret";
 export const mongoUri = process.env.MONGODB_URI || "";
-export const dbName = process.env.DB_NAME || "barbersoft";
+export const dbName = process.env.DB_NAME || "Force";
